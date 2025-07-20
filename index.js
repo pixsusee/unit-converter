2b1a002012caa36c301e32be4becb80c78471c12
Suseendar <pixsusee@gmail.com>
2025-07-20 23:32:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ inputBtn.addEventListener("click", function(){
     let num = document.getElementById("number-input")
     // console.log(num.value)
 
-    lengthEl.innerText = `${num.value} meters = ${(num.value * 3.281.toFixed(3))} feet | ${num.value} feet = ${(num.value / 3.281).toFixed(3)} meters`
+    lengthEl.innerText = `${num.value} meters = ${(num.value * 3.281).toFixed(3)} feet | ${num.value} feet = ${(num.value / 3.281).toFixed(3)} meters`
     volumeEl.innerText = `${num.value} liters = ${(num.value * 0.264).toFixed(3)} gallons | ${num.value} gallons = ${(num.value / 0.264).toFixed(3)} liters`
     massEl.innerText = `${num.value} kilos = ${(num.value * 2.204).toFixed(3)} pounds | ${num.value} pounds = ${(num.value / 2.204).toFixed(3)} kilos`
 
